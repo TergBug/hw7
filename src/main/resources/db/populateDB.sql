58bb96b23cc638861c8d2e3706e7775e35918046
TergBug <tergbug@gmail.com>
2020-01-05 16:12:08 +0200

    Initial commit

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,15 @@
+use javaionio;
+insert into skills(name) values ('Java');
+insert into skills(name) values ('C#');
+insert into skills(name) values ('JDBC');
+insert into accounts(name, status) values ('LiXiao', 'ACTIVE');
+insert into accounts(name, status) values ('Din', 'DELETED');
+insert into accounts(name, status) values ('Geek', 'BANNED');
+insert into developers(first_name, last_name, account_id) values ('Din', 'Ford', '2');
+insert into developers(first_name, last_name, account_id) values ('Xiaoming', 'Li', '1');
+insert into developers(first_name, last_name, account_id) values ('Gird', 'Long', '3');
+insert into developer_skill(developer_id, skill_id) values ('1', '1');
+insert into developer_skill(developer_id, skill_id) values ('1', '3');
+insert into developer_skill(developer_id, skill_id) values ('2', '2');
+insert into developer_skill(developer_id, skill_id) values ('3', '1');
+insert into developer_skill(developer_id, skill_id) values ('3', '2');
