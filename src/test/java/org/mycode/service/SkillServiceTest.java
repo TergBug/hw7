c6ce705d52cb18d4e0133fd16370a92c724bf88a
TergBug <tergbug@gmail.com>
2020-01-25 14:47:02 +0200

    Add JDBC implementation

---
+++ src/test/java/org/mycode/service/SkillServiceTest.java
@@ -0,0 +1,32 @@
+package org.mycode.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SkillServiceTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void changeStorage() {
+    }
+}
